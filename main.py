31653161b8b5773073b3a0d5c4492214ffa37a0a
Natsu Tadama <sultan.ridita@gmail.com>
2023-04-10 16:31:49 +0700

    Revert profile function

--- main.py
+++ main.py
@@ -300,7 +300,83 @@ Joined to {ctx.guild.name}: <t:{userJoined}:F>"""
 )
 async def profile(ctx: interactions.CommandContext, user: int = None, mal_username: str = None, extended: bool = False):
     await ctx.defer()
-    await malProfileSubmit(ctx=ctx, user=user, mal_username=mal_username, extended=extended)
+    userRegistered = MESSAGE_MEMBER_REG_PROFILE
+
+    if (user is not None) and (mal_username is not None):
+        try:
+            raise KeyError
+        except KeyError:
+            sendMessages = ""
+            dcEm = exceptionsToEmbed(returnException(
+                f"{EMOJI_USER_ERROR} **You cannot use both options!** Use either one of `user:` or `mal_username:`, hmph. >:("))
+    else:
+        if mal_username is None:
+            if user is not None:
+                uid = user.id
+            else:
+                uid = ctx.author.id
+            try:
+                if checkIfRegistered(uid):
+                    with open(database, "r") as f:
+                        reader = csv.reader(f, delimiter="\t")
+                        for row in reader:
+                            if row[0] == uid:
+                                jikanStats = await jikan.users(username=row[3], extension='full')
+                                break
+                    malProfile = jikanStats['data']
+
+                    dcEm = generateProfile(
+                        malProfile=malProfile, extend=extended)
+                    if user is None:
+                        sendMessages = ""
+                    elif ctx.author.id == uid:
+                        sendMessages = userRegistered
+                    else:
+                        sendMessages = f"<@{uid}> data:"
+                else:
+                    if user is None:
+                        raise KeyError(
+                            f"{EMOJI_USER_ERROR} Sorry, but to use standalone command, you need to `/register` your account. Or, you can use `/profile mal_username:<yourUsername>` instead")
+                    else:
+                        raise KeyError(
+                            f"I couldn't find <@!{uid}> on my database. It could be that they have not registered their MAL account yet.")
+            except KeyError as regAccount:
+                foo = "Please be a good child, okay? 🚶‍♂️" if user is None else ""
+                sendMessages = ""
+                dcEm = interactions.Embed(
+                    title="User have not registered yet!",
+                    description=returnException(regAccount),
+                    color=0xFF0000,
+                    footer=interactions.EmbedFooter(
+                        text=foo
+                    )
+                )
+            except Exception as e:
+                sendMessages = ""
+                dcEm = exceptionsToEmbed(definejikanException(e))
+        elif mal_username is not None:
+            uname = mal_username.strip()
+            try:
+                with open(database, "r") as f:
+                    reader = csv.reader(f, delimiter="\t")
+                    for row in reader:
+                        if (str(row[3]).lower() == str(uname).lower()) and (ctx.author.id == row[0]):
+                            sendMessages = userRegistered
+                            break
+                        elif (str(row[3]).lower() == str(uname).lower()) and (ctx.author.id != row[0]):
+                            sendMessages = f"{EMOJI_ATTENTIVE} This MAL account is registered on this bot, you could use `/profile user:<@!{row[0]}>` instead"
+                            break
+                        else:
+                            sendMessages = ""
+                jikanStats = await jikan.users(username=uname, extension='full')
+                malProfile = jikanStats['data']
+
+                dcEm = generateProfile(malProfile=malProfile, extend=extended)
+            except Exception as e:
+                sendMessages = ""
+                dcEm = exceptionsToEmbed(definejikanException(e))
+
+    await ctx.send(sendMessages, embeds=dcEm)
 
 
 @bot.command(
--- modules/myanimelist.py
+++ modules/myanimelist.py
@@ -1,9 +1,8 @@
 from modules.simkl import *
 from modules.commons import *
 from modules.kitsu import *
-from modules.anilist import getAniList
-from modules.animeapi import getNatsuAniApi
-from modules.database import checkIfRegistered
+from modules.anilist import *
+from modules.animeapi import *
 
 
 async def checkClubMembership(username) -> dict:
@@ -558,82 +557,3 @@ async def malSubmit(ctx, ani_id: int, mediaType: str = 'anime'):
         trailer = []
     await ctx.send("", embeds=dcEm, components=trailer)
 
-
-async def malProfileSubmit(ctx: interactions.CommandContext, user: int = None, mal_username: str = None, extended: bool = False):
-    userRegistered = MESSAGE_MEMBER_REG_PROFILE
-
-    if (user is not None) and (mal_username is not None):
-        try:
-            raise KeyError
-        except KeyError:
-            sendMessages = ""
-            dcEm = exceptionsToEmbed(returnException(
-                f"{EMOJI_USER_ERROR} **You cannot use both options!** Use either one of `user:` or `mal_username:`, hmph. >:("))
-    else:
-        if mal_username is None:
-            if user is not None:
-                uid = user.id
-            else:
-                uid = ctx.author.id
-            try:
-                if checkIfRegistered(uid):
-                    with open(database, "r") as f:
-                        reader = csv.reader(f, delimiter="\t")
-                        for row in reader:
-                            if row[0] == uid:
-                                jikanStats = await jikan.users(username=row[3], extension='full')
-                                break
-                    malProfile = jikanStats['data']
-
-                    dcEm = generateProfile(
-                        malProfile=malProfile, extend=extended)
-                    if user is None:
-                        sendMessages = ""
-                    elif ctx.author.id == uid:
-                        sendMessages = userRegistered
-                    else:
-                        sendMessages = f"<@{uid}> data:"
-                else:
-                    if user is None:
-                        raise KeyError(
-                            f"{EMOJI_USER_ERROR} Sorry, but to use standalone command, you need to `/register` your account. Or, you can use `/profile mal_username:<yourUsername>` instead")
-                    else:
-                        raise KeyError(
-                            f"I couldn't find <@!{uid}> on my database. It could be that they have not registered their MAL account yet.")
-            except KeyError as regAccount:
-                foo = "Please be a good child, okay? 🚶‍♂️" if user is None else ""
-                sendMessages = ""
-                dcEm = interactions.Embed(
-                    title="User have not registered yet!",
-                    description=returnException(regAccount),
-                    color=0xFF0000,
-                    footer=interactions.EmbedFooter(
-                        text=foo
-                    )
-                )
-            except Exception as e:
-                sendMessages = ""
-                dcEm = exceptionsToEmbed(definejikanException(e))
-        elif mal_username is not None:
-            uname = mal_username.strip()
-            try:
-                with open(database, "r") as f:
-                    reader = csv.reader(f, delimiter="\t")
-                    for row in reader:
-                        if (str(row[3]).lower() == str(uname).lower()) and (ctx.author.id == row[0]):
-                            sendMessages = userRegistered
-                            break
-                        elif (str(row[3]).lower() == str(uname).lower()) and (ctx.author.id != row[0]):
-                            sendMessages = f"{EMOJI_ATTENTIVE} This MAL account is registered on this bot, you could use `/profile user:<@!{row[0]}>` instead"
-                            break
-                        else:
-                            sendMessages = ""
-                jikanStats = await jikan.users(username=uname, extension='full')
-                malProfile = jikanStats['data']
-
-                dcEm = generateProfile(malProfile=malProfile, extend=extended)
-            except Exception as e:
-                sendMessages = ""
-                dcEm = exceptionsToEmbed(definejikanException(e))
-
-    await ctx.send(sendMessages, embeds=dcEm)
