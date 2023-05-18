5c7db44df3e8b425bf9fdfe0910ce9964ad1d510
Natsu Tadama <sultan.ridita@gmail.com>
2023-05-18 09:05:28 +0700

    Add test method for Trakt

---
+++ tests/test_trakt.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import unittest
+
+try:
+    from classes.trakt import Trakt, TraktExtendedMovieStruct
+except ImportError:
+    # add the path to the 'modules' directory to the system path
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    from classes.trakt import Trakt, TraktExtendedMovieStruct
+
+
+class TraktTest(unittest.IsolatedAsyncioTestCase):
+    async def test_get_ids(self):
+        async with Trakt() as trakt:
+            res = await trakt.lookup(media_id="tt1104001", platform=trakt.Platform.IMDB)
+            if res.type == 'movie':
+                self.assertEqual(res.movie.ids.imdb, "tt1104001")
+            elif res.type == 'show':
+                self.assertEqual(res.show.ids.imdb, "tt1104001")
+
+    async def test_get_title_data(self):
+        async with Trakt() as trakt:
+            res: TraktExtendedMovieStruct = await trakt.get_title_data(media_id=12601, media_type=trakt.MediaType.MOVIE)
+            self.assertIsInstance(res, TraktExtendedMovieStruct)
+
+if __name__ == "__main__":
+    unittest.main()
