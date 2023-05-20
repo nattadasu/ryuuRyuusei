fb9cc24fdc870e340b407636da1ca60c42a69d1b
Natsu Tadama <sultan.ridita@gmail.com>
2023-05-20 08:39:57 +0700

    Improve how bot loads cogs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import subprocess
 import sys
 from datetime import datetime as dtime
 from datetime import timezone as tz
@@ -55,52 +54,53 @@ async def main():
 
     This function will be run before the bot starts.
     """
-    try:
-        if SENTRY_DSN:
-            ext = "interactions.ext.sentry"
-            bot.load_extension("interactions.ext.sentry", token=SENTRY_DSN)
-        ext = "interactions.ext.jurigged"
-        bot.load_extension("interactions.ext.jurigged")
-    except Exception as e:
-        print("[Ext] Error while loading system extension: " + ext)
-        print("      " + str(e))
-        print("[Ext] If this error shows up while restart the bot, ignore")
+    print("[Ext] Loading core/bot extensions...")
+    exts: list[str] = [
+        "interactions.ext.sentry",
+        "interactions.ext.jurigged",
+    ]
+    for i, ext in enumerate(exts):
+        i += 1
+        pg = f"[Ext] [{i}/{len(exts)}]"
+        lpg = len(pg)
+        sp  = " " * lpg
+        print(f"{pg} Loading core/bot extension: {ext}")
+        try:
+            if ext == "interactions.ext.sentry" and SENTRY_DSN:
+                bot.load_extension(ext, token=SENTRY_DSN)
+            bot.load_extension(ext)
+        except Exception as e:
+            print(f"{pg} Error while loading system extension: " + ext)
+            print(sp + str(e))
+            print(f"[Ext] If this error shows up while restart the bot, ignore")
     bot.del_unused_app_cmd = True
     bot.sync_interactions = True
     bot.send_command_tracebacks = False
 
-    # git pull
-    print("[Sys] Checking for updates...")
-    subprocess.run(["git", "pull"], check=True)
-
-    from firstRun import first_run
-
-    # get this python binary's path
-    python_path = sys.executable
-    python_path = python_path.replace("\\", "/")
-    await first_run(py_bin=python_path)
-
     # Load extensions
-    print("[Cog] Loading extensions...")
+    print("[Cog] Loading cog/extensions...")
 
     # for each .py files in extensions folder, load it, except for commons.py
-    try:
-        exts = os.listdir("extensions")
-        for i, ext in enumerate(exts):
-            i += 1
+    exts = os.listdir("extensions")
+    for i, ext in enumerate(exts):
+        i += 1
+        pg = f"[Cog] [{i}/{len(exts)}]"
+        lpg = len(pg)
+        sp = " " * lpg
+        try:
             if ext.endswith(".py"):
-                print(f"[Cog] [{i}/{len(exts)}] Loading cog/extension: {ext}")
+                print(f"{pg} Loading cog/extension: {ext}")
                 ext = ext[:-3]
                 if ext != "commons":
                     bot.load_extension("extensions." + ext)
                 else:
                     bot.load_extension("extensions." + ext, now=now)
             else:
-                print(f"[Cog] [{i}/{len(exts)}] Skipping: {ext}, not a .py file")
-    except Exception as e:
-        print(f"[Cog] [{i}/{len(exts)}] Error while loading extension: {ext}")
-        print("      " + str(e))
-        print("[Cog] If this error shows up while restart the bot, ignore")
+                print(f"{pg} Skipping: {ext}, not a .py file")
+        except Exception as e:
+            print(f"{pg} Error while loading extension: {ext}")
+            print({sp} + str(e))
+            print("[Cog] If this error shows up while restart the bot, ignore")
 
     await bot.astart()
 
