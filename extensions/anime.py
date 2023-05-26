67b672e98fa2c60d8ec25de633c17dec04396bf5
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-05-26 16:11:15 +0700

    Optimize anime search command

--- extensions/anime.py
+++ extensions/anime.py
@@ -47,7 +47,7 @@ class Anime(ipy.Extension):
         self, ctx: ipy.SlashContext, query: str, provider: str = "anilist"
     ):
         await ctx.defer()
-        ul = read_user_language(ctx)
+        ul: str = read_user_language(ctx)
         l_: LanguageDict = fetch_language_data(ul, useRaw=True)
         send = await ctx.send(
             embed=ipy.Embed(
@@ -58,100 +58,92 @@ class Anime(ipy.Extension):
                 ),
             )
         )
-        f = []
-        so = []
         try:
             if provider == "anilist":
                 res = await search_al_anime(title=query)
-                if res is None or len(res) == 0:
-                    raise Exception("No result")
             elif provider == "mal":
                 res = await searchMalAnime(title=query)
-                if res is None or len(res) == 0:
-                    raise Exception("No result")
+            if not res:
+                raise Exception("No result")
+            f: list[ipy.EmbedField] = []
+            so: list[ipy.StringSelectOption] = []
             for a in res:
-                a = a["node"]
-                if a["start_season"] is None:
-                    a["start_season"] = {"season": "Unknown", "year": "Year"}
-                media_type: str = a["media_type"].lower()
-                try:
-                    media_type = l_["commons"]["media_formats"][media_type]
-                except KeyError:
-                    media_type = l_["commons"]["unknown"]
+                if a["node"]["start_season"] is None:
+                    a["node"]["start_season"] = {"season": "Unknown", "year": "Year"}
+                media_type: str = (
+                    l_["commons"]["media_formats"].get(
+                        a["node"]["media_type"].lower(),
+                        l_["commons"]["unknown"],
+                    )
+                )
                 season: str = (
-                    a["start_season"]["season"]
-                    if a["start_season"]["season"]
-                    else "unknown"
+                    l_["commons"]["season"].get(
+                        a["node"]["start_season"]["season"], "unknown"
+                    )
                 )
-                season = l_["commons"]["season"][season]
-                year = (
-                    a["start_season"]["year"]
-                    if a["start_season"]["year"]
-                    else l_["commons"]["year"]["unknown"]
+                year: str = (
+                    a["node"]["start_season"]["year"]
+                    or l_["commons"]["year"]["unknown"]
                 )
-                title = a["title"]
-                mdTitle = sanitize_markdown(title)
-                alt = a["alternative_titles"]
-                if alt is not None and alt["ja"] is not None:
-                    native = sanitize_markdown(alt["ja"])
-                    native += "\n"
-                else:
-                    native = ""
-                f += [
+                title: str = a["node"]["title"]
+                if len(title) > 256:
+                    title = title[:253] + "..."
+                mdTitle: str = sanitize_markdown(title)
+                alt = a["node"]["alternative_titles"]
+                native: str = sanitize_markdown(alt["ja"]) + "\n" if alt and alt["ja"] else ""
+                f.append(
                     ipy.EmbedField(
-                        name=mdTitle[:253] + ("..." if len(mdTitle) > 253 else ""),
-                        value=f"{native}`{a['id']}`, {media_type}, {season} {year}",
+                        name=mdTitle,
+                        value=f"{native}`{a['node']['id']}`, {media_type}, {season} {year}",
                         inline=False,
                     )
-                ]
-                so += [
+                )
+                so.append(
                     ipy.StringSelectOption(
-                        # trim to 80 chars in total
                         label=title[:77] + ("..." if len(title) > 77 else ""),
-                        value=a["id"],
+                        value=a["node"]["id"],
                         description=f"{media_type}, {season} {year}",
                     )
-                ]
-            if len(f) >= 1:
-                title = l_["commons"]["search"]["result_title"].format(QUERY=query)
-                if provider == "anilist":
-                    title += " (AniList)"
-                result = genrate_search_embed(
-                    title=title,
-                    language=ul,
-                    mediaType="anime",
-                    query=query,
-                    platform="MyAnimeList",
-                    homepage="https://myanimelist.net",
-                    results=f,
-                    icon="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png",
-                    color=0x2F51A3,
                 )
-                await send.edit(
-                    content="",
-                    embed=result,
-                    components=ipy.ActionRow(
-                        ipy.StringSelectMenu(
-                            *so, placeholder="Choose an anime", custom_id="mal_search"
-                        )
-                    ),
-                )
+            title = l_["commons"]["search"]["result_title"].format(QUERY=query)
+            if provider == "anilist":
+                title += " (AniList)"
+            result = generate_search_embed(
+                title=title,
+                language=ul,
+                mediaType="anime",
+                query=query,
+                platform="MyAnimeList",
+                homepage="https://myanimelist.net",
+                results=f,
+                icon="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png",
+                color=0x2F51A3,
+            )
+            await send.edit(
+                embed=result,
+                components=ipy.ActionRow(
+                    ipy.StringSelectMenu(
+                        *so, placeholder="Choose an anime", custom_id="mal_search"
+                    )
+                ),
+            )
             await asyncio.sleep(60)
             await send.edit(components=[])
-        except Exception:
-            l_ = l_["strings"]["anime"]["search"]["exception"]
-            emoji = rSub(r"(<:.*:)(\d+)(>)", r"\2", EMOJI_UNEXPECTED_ERROR)
+        except Exception as e:
+            l_: dict[str, str] = l_["strings"]["anime"]["search"]["exception"]
+            emoji = EMOJI_UNEXPECTED_ERROR.split(":")[2].split(">")[0]
             embed = ipy.Embed(
                 title=l_["title"],
-                description=l_["text"].format(
-                    QUERY=query,
-                ),
+                description=l_["text"].replace(
+                    "MyAnimeList",
+                    "AniList"
+                    if provider == "anilist"
+                    else "MyAnimeList"
+                ).format(QUERY=f"`{query}`"),
                 color=0xFF0000,
                 footer=ipy.EmbedFooter(text=l_["footer"]),
             )
-            embed.set_thumbnail(
-                url=f"https://cdn.discordapp.com/emojis/{emoji}.png?v=1"
-            )
+            embed.set_thumbnail(url=f"https://cdn.discordapp.com/emojis/{emoji}.png?v=1")
             await send.edit(embed=embed)
 
     @ipy.component_callback("mal_search")
