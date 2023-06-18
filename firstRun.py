54733a4babf06fdbdfe605c88037a3b2eccaf644
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-06-18 22:01:57 +0000

    Add exception handle if pip fails

--- firstRun.py
+++ firstRun.py
@@ -34,33 +34,40 @@ async def first_run(py_bin: str = py_bin_path()):
     """
     # Check if the script is run from the root directory
     if not os.path.exists("requirements.txt"):
-        raise Exception("Please run the script from the root directory.")
-    # Check if Termux is used
-    env = {"MATHLAB": "m"} if check_termux() else {}
-    # Install dependencies
-    print(
-        "Installing and upgrading dependencies for the next step and the bot itself..."
-    )
-    if current_os() == "Windows":
-        subprocess.run(
-            ["pip", "install", "-U", "-r", "requirements.txt"],
-            check=True,
+        raise Exception("Please run the script from the repo's directory.")
+    try:
+        # Check if Termux is used
+        env = {"MATHLAB": "m"} if check_termux() else {}
+        # Install dependencies
+        print(
+            "Installing and upgrading dependencies for the next step and the bot itself..."
         )
-    else:
-        subprocess.run(
-            [
-                shlex.quote(py_bin),
-                "-m",
-                "pip",
-                "install",
-                "-U",
-                "-r",
-                "requirements.txt",
-            ],
-            env=env,
-            check=True,
-        )
-    # Prepare the database
+        if current_os() == "Windows":
+            subprocess.run(
+                ["pip", "install", "-U", "-r", "requirements.txt"],
+                check=True,
+            )
+        else:
+            subprocess.run(
+                [
+                    shlex.quote(py_bin),
+                    "-m",
+                    "pip",
+                    "install",
+                    "-U",
+                    "-r",
+                    "requirements.txt",
+                ],
+                env=env,
+                check=True,
+            )
+        # Prepare the database
+    except Exception:
+        print("\033[31mError installing packages, please run frollowing command:")
+        command = "pip install -U -r requirements.txt"
+        if check_termux():
+            commmand = "MATHLAB=m " + command
+        print(f"{command}\033[0m")
     print("Preparing the database as database.csv in tabbed format...")
     prepare_database()
     # Fetch data from GitHub
