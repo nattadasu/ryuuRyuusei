5878bb71935483c5e0bac2deae8810765a009f19
Sultan Iskandar Maulana <nattadasu@nattadasu.my.id>
2023-06-27 08:45:00 +0000

    Resolve PTC-W0052 error and typing issues

--- extensions/movies.py
+++ extensions/movies.py
@@ -1,4 +1,11 @@
-from typing import Literal
+"""
+Movies commands
+
+Available commands:
+- /movies search <query> - Search for movies
+- /movies info <media_id> - Get information about a movie
+- /movies random - Get a random movie
+"""
 
 import interactions as ipy
 
@@ -11,7 +18,7 @@ from modules.i18n import fetch_language_data, read_user_language
 from modules.simkl import simkl_submit
 
 
-class Movies(ipy.Extension):
+class MoviesCog(ipy.Extension):
     """Movies commands"""
 
     movies = ipy.SlashCommand(
@@ -23,6 +30,7 @@ class Movies(ipy.Extension):
             interval=10,
         ),
     )
+    """Movie command initialiser"""
 
     @movies.subcommand(
         sub_cmd_name="search",
@@ -37,21 +45,29 @@ class Movies(ipy.Extension):
         ],
     )
     async def movie_search(self, ctx: ipy.SlashContext, query: str):
+        """
+        Search for movies
+
+        Parameters
+        ----------
+        query : str
+            The movie title to search for
+        """
         await ctx.defer()
-        ul: str = read_user_language(ctx)
-        l_ = fetch_language_data(ul, use_raw=True)
+        user_lang: str = read_user_language(ctx)
+        language_dict = fetch_language_data(user_lang, use_raw=True)
         send = await ctx.send(
             embed=ipy.Embed(
-                title=l_["commons"]["search"]["init_title"],
-                description=l_["commons"]["search"]["init"].format(
+                title=language_dict["commons"]["search"]["init_title"],
+                description=language_dict["commons"]["search"]["init"].format(
                     QUERY=query,
                     PLATFORM="SIMKL",
                 ),
             )
         )
         try:
-            f: list[ipy.EmbedField] = []
-            so: list[ipy.StringSelectOption] = []
+            fields: list[ipy.EmbedField] = []
+            select_options: list[ipy.StringSelectOption] = []
             async with Simkl() as simkl:
                 results = await simkl.search_by_title(
                     title=query, limit=5, media_type=SimklMediaTypes.MOVIE
@@ -66,37 +82,37 @@ class Movies(ipy.Extension):
                 if len(title) >= 256:
                     title = title[:253] + "..."
                 md_title = sanitize_markdown(title)
-                f.append(
+                fields.append(
                     ipy.EmbedField(
                         name=f"{md_title}",
                         value=f"`{media_id}`\n{first_aired}",
                         inline=False,
                     )
                 )
-                so.append(
+                select_options.append(
                     ipy.StringSelectOption(
                         label=f"{md_title}",
                         value=f"{media_id}",
                         description=f"{first_aired}",
                     )
                 )
-            title = l_["commons"]["search"]["result_title"].format(
+            title = language_dict["commons"]["search"]["result_title"].format(
                 QUERY=query,
             )
             result_embed = generate_search_embed(
-                language=ul,
+                language=user_lang,
                 mediaType="movies",
                 query=query,
                 platform="SIMKL",
                 homepage="https://simkl.com",
                 title=title,
-                results=f,
+                results=fields,
                 icon="https://media.discordapp.net/attachments/1078005713349115964/1094570318967865424/ico_square_1536x1536.png",
                 color=0x0B0F10,
             )
             components = ipy.spread_to_rows(
                 ipy.StringSelectMenu(
-                    *so,
+                    *select_options,
                     placeholder="Choose a movie",
                     custom_id="simkl_search_select_movie",
                 ),
@@ -111,14 +127,15 @@ class Movies(ipy.Extension):
                 embed=result_embed,
                 components=components,
             )
+        # pylint: disable=broad-except
         except Exception as _:
-            l_: dict[str, str] = l_["strings"]["movies"]["search"]["exception"]
+            language_dict: dict[str, str] = language_dict["strings"]["movies"]["search"]["exception"]
             emoji = EMOJI_UNEXPECTED_ERROR.split(":")[2].split(">")[0]
             embed = ipy.Embed(
-                title=l_["title"],
-                description=l_["text"].format(QUERY=f"`{query}`"),
+                title=language_dict["title"],
+                description=language_dict["text"].format(QUERY=f"`{query}`"),
                 color=0xFF0000,
-                footer=ipy.EmbedFooter(text=l_["footer"]),
+                footer=ipy.EmbedFooter(text=language_dict["footer"]),
             )
             embed.set_thumbnail(
                 url=f"https://cdn.discordapp.com/emojis/{emoji}.png?v=1"
@@ -133,9 +150,11 @@ class Movies(ipy.Extension):
                 ),
             )
             save_traceback_to_file("movies_search", ctx.author, _)
+        # pylint: enable=broad-except
 
     @ipy.component_callback("simkl_search_select_movie")
     async def simkl_search_select_movie(self, ctx: ipy.ComponentContext):
+        """Callback for the SIMKL search select menu"""
         await ctx.defer()
         entry_id: int = int(ctx.values[0])
         await simkl_submit(ctx, entry_id, "movies")
@@ -161,12 +180,27 @@ class Movies(ipy.Extension):
         ],
     )
     async def movie_info(self, ctx: ipy.SlashContext, media_id: str):
+        """
+        Get information about a movie
+
+        Parameters
+        ----------
+        media_id : str
+            The movie ID on SIMKL or IMDB to get the data from
+        """
         await ctx.defer()
         await simkl_submit(ctx, media_id, "movies")
 
     @movies.subcommand(sub_cmd_name="random",
                        sub_cmd_description="Get a random movie")
     async def movie_random(self, ctx: ipy.SlashContext):
+        """
+        Get a random movie
+
+        Parameters
+        ----------
+        None
+        """
         await ctx.defer()
         send = await ctx.send(
             embed=ipy.Embed(
@@ -206,4 +240,5 @@ class Movies(ipy.Extension):
 
 
 def setup(bot: ipy.Client | ipy.AutoShardedClient):
-    Movies(bot)
+    """Load the Movies cog."""
+    MoviesCog(bot)
