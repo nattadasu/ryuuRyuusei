4ab77d70a222c34af1876fb031e3bc80b7991469
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-11-29 09:42:05 +0700

    Add Urban Dictionary command

--- classes/urbandictionary.py
+++ classes/urbandictionary.py
@@ -1,13 +1,19 @@
 """# Urban Dictionary Unofficial API Wrapper"""
 
+import re
 from dataclasses import dataclass
 from datetime import datetime
+from urllib.parse import quote
 
 import aiohttp
+from interactions import Embed
 from bs4 import BeautifulSoup
+from fake_useragent import FakeUserAgent
 
 from classes.excepts import ProviderHttpError
-from modules.const import USER_AGENT
+
+
+USER_AGENT = FakeUserAgent().random
 
 
 @dataclass
@@ -35,6 +41,44 @@ class UrbanDictionaryEntry:
     thumbs_down: int
     """Number of thumbs down"""
 
+    @property
+    def embed(self) -> Embed:
+        """
+        Get embed of the word
+
+        Returns:
+            Embed: Embed of the word
+        """
+        embed = Embed(
+            title=self.word,
+            url=self.permalink,
+            description=f"{self.definition}",
+            color=0x1b2936,
+        )
+        embed.add_field(
+            name="Example",
+            value=self.example,
+            inline=False,
+        )
+        embed.add_field(
+            name="Author",
+            value=self.author,
+            inline=True,
+        )
+        embed.set_author(
+            name="Urban Dictionary",
+            url="https://www.urbandictionary.com/",
+        )
+        # percentage of thumbs up
+        percentage = round(
+            (self.thumbs_up / (self.thumbs_up + self.thumbs_down)) * 100
+        )
+        embed.set_footer(
+            text=f"👍 {self.thumbs_up} / {self.thumbs_down} ({percentage}%)"
+        )
+        embed.timestamp = self.written_on  # type: ignore
+        return embed
+
 
 class UrbanDictionary:
     """Urban Dictionary Unofficial API Wrapper"""
@@ -51,13 +95,37 @@ class UrbanDictionary:
         self.header = {"User-Agent": USER_AGENT}
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):  # type: ignore
         """Async exit"""
         await self.close()
 
     async def close(self):
         """Close session"""
-        await self.session.close()
+        await self.session.close() if self.session else None
+
+    @staticmethod
+    def _add_hyperlinks(text: str) -> str:
+        """
+        Add hyperlinks to text if Urban Dictionary has wiki markup
+
+        Args:
+            text (str): Text to check
+
+        Returns:
+            str: Replaced text
+        """
+        ud_link_pattern = re.compile(r'\[([^\]]+)\]')
+
+        # Find all matches in the text
+        matches = ud_link_pattern.findall(text)
+
+        # Replace each Urban Dictionary hyperlink with Markdown format
+        for match in matches:
+            urban_dictionary_link = f'https://www.urbandictionary.com/define.php?term={quote(match)}'
+            markdown_link = f'[{match}]({urban_dictionary_link})'
+            text = text.replace(f'[{match}]', markdown_link)
+
+        return text
 
     async def lookup_definition(self, term: str) -> list[UrbanDictionaryEntry]:
         """
@@ -69,6 +137,8 @@ class UrbanDictionary:
         Returns:
             list[UrbanDictionaryEntry]: List of UrbanDictionaryEntry
         """
+        if self.session is None:
+            raise Exception("Urban Dictionary session not initialized")
         params = {"term": term}
         async with self.session.get(
             f"{self.base_url}/define", headers=self.header, params=params
@@ -84,6 +154,9 @@ class UrbanDictionary:
                     f"{term} not found in Urban Dictionary", 404)
             listed: list[UrbanDictionaryEntry] = []
             for entry in data["list"]:
+                # convert wiki markup to markdown
+                entry["definition"] = self._add_hyperlinks(entry["definition"])
+                entry["example"] = self._add_hyperlinks(entry["example"])
                 entry["written_on"].replace("Z", "+00:00")
                 entry["written_on"] = datetime.fromisoformat(
                     entry["written_on"])
@@ -101,6 +174,8 @@ class UrbanDictionary:
         Returns:
             str: Raw HTML of Urban Dictionary
         """
+        if self.session is None:
+            raise Exception("Urban Dictionary session not initialized")
         async with self.session.get(
             f"https://www.urbandictionary.com/{path}", headers=self.header
         ) as resp:
@@ -139,13 +214,15 @@ class UrbanDictionary:
         html = await self._fetch_raw_html()
         return await self._parse_html(html)
 
-    async def get_random_word(self) -> UrbanDictionaryEntry:
+    async def get_random_word(self) -> list[UrbanDictionaryEntry]:
         """
         Get random word
 
         Returns:
-            UrbanDictionaryEntry: Random word
+            list[UrbanDictionaryEntry]: Random word
         """
+        if self.session is None:
+            raise Exception("Urban Dictionary session not initialized")
         async with self.session.get(
             f"{self.base_url}/random", headers=self.header
         ) as resp:
@@ -155,7 +232,14 @@ class UrbanDictionary:
                     resp.status,
                 )
             data = await resp.json()
-            entry: dict[str, str | int] = data["list"][0]
-            entry["written_on"].replace("Z", "+00:00")
-            entry["written_on"] = datetime.fromisoformat(entry["written_on"])
-            return UrbanDictionaryEntry(**entry)
+            listed: list[UrbanDictionaryEntry] = []
+            for entry in data["list"]:
+                # convert wiki markup to markdown
+                entry["definition"] = self._add_hyperlinks(entry["definition"])
+                entry["example"] = self._add_hyperlinks(entry["example"])
+                entry["written_on"].replace("Z", "+00:00")
+                entry["written_on"] = datetime.fromisoformat(
+                    entry["written_on"])
+                listed += [UrbanDictionaryEntry(**entry)]
+
+            return listed
---
+++ extensions/urban.py
@@ -0,0 +1,142 @@
+"""Add Urban Dictionary commands to the bot."""
+
+import interactions as ipy
+import aiohttp
+from interactions.ext.paginators import Paginator
+
+from classes.urbandictionary import UrbanDictionary as Urban, UrbanDictionaryEntry as Entry
+from modules.commons import platform_exception_embed, save_traceback_to_file
+
+
+class UrbanDictionaryCog(ipy.Extension):
+    """Urban Dictionary Cog"""
+
+    urban_head = ipy.SlashCommand(
+        name="urban",
+        description="Search Urban Dictionary for a word or phrase.",
+        nsfw=True,
+    )
+
+    @urban_head.subcommand(
+        sub_cmd_name="random",
+        sub_cmd_description="Get a random word or phrase from Urban Dictionary.",
+        options=[
+            ipy.SlashCommandOption(
+                name="limit",
+                description="Maximum number of results to return.",
+                type=ipy.OptionType.INTEGER,
+                required=False,
+                min_value=1,
+                max_value=10,
+            ),
+        ],
+        nsfw=True,
+    )
+    async def urban_random(self, ctx: ipy.SlashContext, limit: int = 10):
+        """Get a random word or phrase from Urban Dictionary."""
+        await ctx.defer()
+        entry: list[Entry] | None = None
+        try:
+            async with Urban() as ud:
+                entry = await ud.get_random_word()
+        except Exception as e:
+            await ctx.send(embed=platform_exception_embed(
+                description="Failed to get random word from Urban Dictionary.",
+                error=f"{e}",
+            ))
+            save_traceback_to_file("urban_random", ctx.author, e)
+            return
+        pages: list[ipy.Embed] = []
+        for _, e in enumerate(entry[:limit]):
+            pages.append(e.embed)
+        paginator = Paginator.create_from_embeds(self.bot, *pages, timeout=30)
+        await paginator.send(ctx)  # type: ignore
+
+    @urban_head.subcommand(
+        sub_cmd_name="search",
+        sub_cmd_description="Search Urban Dictionary for a word or phrase.",
+        options=[
+            ipy.SlashCommandOption(
+                name="term",
+                description="Word or phrase to search for.",
+                type=ipy.OptionType.STRING,
+                required=True,
+                autocomplete=True,
+            ),
+            ipy.SlashCommandOption(
+                name="limit",
+                description="Maximum number of results to return.",
+                type=ipy.OptionType.INTEGER,
+                required=False,
+                min_value=1,
+                max_value=10,
+            ),
+        ],
+        nsfw=True,
+    )
+    async def urban_search(self, ctx: ipy.SlashContext, term: str, limit: int = 10):
+        """Search Urban Dictionary for a word or phrase."""
+        await ctx.defer()
+        entry: list[Entry] | None = None
+        try:
+            async with Urban() as ud:
+                entry = await ud.lookup_definition(term)
+        except Exception as e:
+            await ctx.send(embed=platform_exception_embed(
+                description=f"Failed to search Urban Dictionary for `{term}`.",
+                error=f"{e}",
+            ))
+            save_traceback_to_file("urban_random", ctx.author, e)
+            return
+
+        pages: list[ipy.Embed] = []
+        for _, e in enumerate(entry[:limit]):
+            pages.append(e.embed)
+        paginator = Paginator.create_from_embeds(self.bot, *pages, timeout=30)
+        await paginator.send(ctx)  # type: ignore
+
+    @urban_search.autocomplete(option_name="term")
+    async def urban_search_autocomplete(self, ctx: ipy.AutocompleteContext):
+        """Autocomplete Urban Dictionary search term."""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.urbandictionary.com/v0/autocomplete-extra?term={ctx.input_text}") as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                    results: list[dict[str, str]] = []
+                    for d in data["results"][:25]:
+                        results.append({
+                            "name": d["term"][:80],
+                            "value": d["term"],
+                        })
+                    await ctx.send(choices=results)  # type: ignore
+                else:
+                    await ctx.send(choices=[{
+                        "name": f"{ctx.input_text}",
+                        "value": f"{ctx.input_text}",
+                    }])
+
+    @urban_head.subcommand(
+        sub_cmd_name="wotd",
+        sub_cmd_description="Get the Urban Dictionary Word of the Day.",
+        nsfw=True,
+    )
+    async def urban_wotd(self, ctx: ipy.SlashContext):
+        """Get the Urban Dictionary Word of the Day."""
+        await ctx.defer()
+        entry: Entry | None = None
+        try:
+            async with Urban() as ud:
+                entry = await ud.get_word_of_the_day()
+        except Exception as e:
+            await ctx.send(embed=platform_exception_embed(
+                description="Failed to get Urban Dictionary Word of the Day.",
+                error=f"{e}",
+            ))
+            save_traceback_to_file("urban_random", ctx.author, e)
+            return
+        await ctx.send(embed=entry.embed) # type: ignore
+
+
+def setup(bot: ipy.Client):
+    """Add the Urban Dictionary Cog to the bot."""
+    UrbanDictionaryCog(bot)
