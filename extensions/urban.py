5c0de5dfcf17c4e2955f28ae2a42d917784318b5
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-12-11 14:53:11 +0700

    Add method to calculate truthiness of an entry in UD
    So it recommends most likely relevant to the query, then total upvotes

--- extensions/urban.py
+++ extensions/urban.py
@@ -2,13 +2,25 @@
 
 import aiohttp
 import interactions as ipy
+import re
 from interactions.ext.paginators import Paginator
+from typing import TypedDict
 
 from classes.urbandictionary import UrbanDictionary as Urban
 from classes.urbandictionary import UrbanDictionaryEntry as Entry
 from modules.commons import (get_nsfw_status, platform_exception_embed,
                              save_traceback_to_file)
 
+
+class TrustModel(TypedDict):
+    """Trust Model for Urban Dictionary entries."""
+
+    entry: Entry
+    """Entry"""
+    maxratio: float
+    """Max ratio of thumbs up to max thumbs up"""
+    trustratio: float
+    """Trust ratio"""
 
 class UrbanDictionaryCog(ipy.Extension):
     """Urban Dictionary Cog"""
@@ -33,6 +45,70 @@ class UrbanDictionaryCog(ipy.Extension):
         paginator = Paginator.create_from_embeds(self.bot, *pages, timeout=30)
         stat = await paginator.send(ctx)
         return stat
+
+    @staticmethod
+    def _sort_by_thumbs_up(entries: list[Entry]) -> list[Entry]:
+        """
+        Sort list of Urban Dictionary entries by thumbs up.
+
+        Args:
+            entries (list[Entry]): List of Urban Dictionary entries
+
+        Returns:
+            list[Entry]: Sorted list of Urban Dictionary entries
+        """
+        # get the most thumbs up
+        thumbs_up = sorted(entries, key=lambda e: e.thumbs_up, reverse=True)
+        max_thumbs_up = thumbs_up[0].thumbs_up
+        trusted_list: list[TrustModel] = []
+
+        # Separate trusted and untrusted entries
+        for e in thumbs_up:
+            trustratio = e.thumbs_up / (e.thumbs_up + e.thumbs_down)
+            if trustratio >= 0.7:
+                trusted_list.append({
+                    "entry": e,
+                    "maxratio": e.thumbs_up / max_thumbs_up,
+                    "trustratio": trustratio,
+                })
+
+        # Sort trusted entries by thumbs up
+        trusted_entries = sorted(trusted_list, key=lambda t: t["entry"].thumbs_up, reverse=True)
+        trusted_entries = [t["entry"] for t in trusted_entries]
+
+        # Sort untrusted entries by thumbs up
+        untrusted_entries = [e for e in thumbs_up if e not in trusted_entries]
+
+        # If entry is < 70% trusted, put it at the bottom of the list
+        untrusted_entries = sorted(untrusted_entries, key=lambda e: e.thumbs_up, reverse=True)
+
+        # Merge trusted and untrusted entries
+        sorted_entries = trusted_entries + untrusted_entries
+
+        return sorted_entries
+
+    @staticmethod
+    def _fix_footer(embed: ipy.Embed, cursor: int = 1, total: int = 10) -> ipy.Embed:
+        """
+        Fix the footer of an Urban Dictionary embed.
+
+        Args:
+            embed (ipy.Embed): Embed to fix
+            cursor (int, optional): Current page. Defaults to 1.
+            total (int, optional): Total number of pages. Defaults to 10.
+
+        Returns:
+            ipy.Embed: Fixed embed
+        """
+        foo = embed.footer
+        if not foo:
+            return embed
+        current = foo.text
+        # replace current footer with fixed one with following format:
+        # page 1/10 -> page 1 of 10
+        replace = re.sub(r"(\d+)/(\d+)", f"{cursor} of {total}", current)
+        embed.set_footer(text=replace)
+        return embed
 
     @urban_head.subcommand(
         sub_cmd_name="random",
@@ -65,8 +141,10 @@ class UrbanDictionaryCog(ipy.Extension):
             save_traceback_to_file("urban_random", ctx.author, e)
             return
         pages: list[ipy.Embed] = []
-        for _, e in enumerate(entry[:limit]):
-            pages.append(e.embed)
+        entry_lim = self._sort_by_thumbs_up(entry[:limit])
+        for pg, e in enumerate(entry_lim):
+            embed = self._fix_footer(e.embed, cursor=pg + 1, total=len(entry_lim))
+            pages.append(embed)
         await self._paginator(ctx, pages)
 
     @urban_head.subcommand(
@@ -108,8 +186,10 @@ class UrbanDictionaryCog(ipy.Extension):
             return
 
         pages: list[ipy.Embed] = []
-        for _, e in enumerate(entry[:limit]):
-            pages.append(e.embed)
+        entry_lim = self._sort_by_thumbs_up(entry[:limit])
+        for pg, e in enumerate(entry_lim):
+            embed = self._fix_footer(e.embed, cursor=pg + 1, total=len(entry_lim))
+            pages.append(embed)
         paginator = Paginator.create_from_embeds(self.bot, *pages, timeout=30)
         await paginator.send(ctx)  # type: ignore
 
